59d3627398d90c5aa1d8484b339ff2cb0c1c3e1b
Brian Daniels <brian@brianjdaniels.com>
2013-08-03 02:49:42 +0000

    Added quiz stuff to serve from web.js

--- web.js
+++ web.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express.createServer(express.logger());
+app.use(express.static(path.join(__dirname, 'public')));
 var html = fs.readFileSync('index.html').toString();
+var quiz = fs.readFileSync('../quiz/myQuiz.html').toString();
 
 app.get('/', function(request, response) {
   response.send(html);
+});
+
+app.get('/quiz', function(request, response) {
+  response.send(quiz);
 });
 
 var port = process.env.PORT || 8080;
